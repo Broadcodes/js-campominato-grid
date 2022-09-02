fd7a13c5359b66fe53f5b78aef14d79809d3e7cf
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-09-02 14:39:27 +0200

    Aggiunto evento al pulsante button

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro se il numero progressivo è pari,
+di verde se dispari. Inoltre emette un messaggio in console con il numero della cella cliccata.
+*/
+
+// Recupero l'elemento pulsante dal HTML
+const button = document.getElementById("play");
+
+// Aggiungo l'evento al pulsante button
+button.addEventListener("click", function(){
+
+    console.log("ok");
+
+});
